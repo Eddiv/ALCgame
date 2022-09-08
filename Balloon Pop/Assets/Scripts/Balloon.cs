1fe7401fdcbeba371a3a01d94bb52b2d0bf8b8f9
Ethan <ethandivine068@gmail.com>
2022-09-08 09:22:18 -0600

    Created New Game "Balloon Pop"

---
+++ Balloon Pop/Assets/Scripts/Balloon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    public int scoreToGive = 100; //Score to give for the popped balloon.
+    public int clickToPop = 3; //How many clicks before balloon pops
+    public float scaleToIncrease = 0.10f; //Scale to increase each time the ballon is clicked
+
+    //public ScoreManager scoreManager; // A variable to refrence the scoreManager
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reference ScoreManager Component
+        //scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+    void OnMouseDown() {
+        clickToPop -= 1;
+        transform.localScale += Vector3.one * scaleToIncrease;
+
+        if(clickToPop == 0) {
+            //scoreManager.IncreaseScoreText(scoreToGive);
+            Destroy(gameObject);
+        };
+    }
+}
