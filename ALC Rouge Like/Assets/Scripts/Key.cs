968617ec8a7d7dd6588d4d1ce86c0a049b174d72
Ethan <ethandivine068@gmail.com>
2022-10-05 09:18:04 -0600

    Worked on Pickups

---
+++ ALC Rouge Like/Assets/Scripts/Key.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : Pickup
+{
+    private GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.Find("GameManager").
+        GetComponent<GameManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gm.AddKey(amount);
+            Destroy(gameObject);            
+        }
+    }
+}
---
+++ ALC Rouge Like/Assets/Scripts/Pickup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public string pickupType;
+    public string itemName;
+    public string description;
+    public int amount;
+    public int sellValue;
+    public int buyValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+   
+}
