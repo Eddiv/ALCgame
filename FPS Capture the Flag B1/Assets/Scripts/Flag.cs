788067fe387b8a0ac4809795fb39447faa734f24
Ethan <ethandivine068@gmail.com>
2022-12-12 09:09:01 -0700

    Worked on Capture the Flag

--- FPS Capture the Flag B1/Assets/Scripts/Flag.cs
+++ FPS Capture the Flag B1/Assets/Scripts/Flag.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Flag : MonoBehaviour
 {
+    private GameManager gm;
+    private Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnTriggerEnter(Collider other) 
     {
-        
+        if(other.CompareTag("Player"))
+        {
+            gm.hasFlag = true;
+            Debug.Log("You have captured the flag");
+            rend.enabled = false;
+        }
     }
 }
